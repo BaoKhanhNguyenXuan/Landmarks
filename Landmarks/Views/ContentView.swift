f82943c1f423a1c3e65072adb925fc5e0f7de4ed
BaoKhanhBG <baokhanh.nguyen@supenient.vn>
2022-02-15 14:08:23 +0700

    App Design and Layout: - Composing Complex Interfaces - Working with UI Controls

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        LandmarkList()
-            .environmentObject(ModelData())
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tag(Tab.featured)
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+            LandmarkList()
+                .tag(Tab.list)
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
         }
     }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
